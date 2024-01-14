09281de0cbe50bf60d3f70a283cfaae9a76e4500
Link Lin <linklin830912@gmail.com>
2024-01-14 15:18:04 +0800

    20240114-three

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import RenderCanvas from "./components/RenderCanvas";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="w-[100vw] h-[100vh] flex items-center justify-center relative">
+      <div className="top-0 left-0 w-[75%] h-[75%] rounded-md">
+        <RenderCanvas />
+      </div>
+    </div>
+  );
 }
 
 export default App;
