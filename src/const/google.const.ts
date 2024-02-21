0219f25fc9d75e12786e12dd6ef20efe6599082e
Link Lin <linklin830912@gmail.com>
2024-02-21 19:59:00 +1100

    20240224 debug

--- src/const/google.const.ts
+++ src/const/google.const.ts
@@ -1,1 +1,2 @@
-export const GOOGLE_FONT_API_KEY = "AIzaSyCOKuKComwJa4hu6LSdU0xZ9uxFnlX_mLE";
+export const GOOGLE_FONT_API_KEY =
+  "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyCOKuKComwJa4hu6LSdU0xZ9uxFnlX_mLE";
